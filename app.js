fdacbe4f5694a4a036bf4e313af7152db798b41e
Jarold Wong <jaroldw@gmail.com>
2017-02-07 23:39:42 +0000

    Sanitize form input

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer'),
     mongoose = require('mongoose');
     
 // APP CONFIG
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -43,6 +45,7 @@ app.get('/blogs/new', function(req, res){
 
 // CREATE
 app.post('/blogs', function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -76,6 +79,7 @@ app.get('/blogs/:id/edit', function(req, res){
 
 // UPDATE ROUTE
 app.put('/blogs/:id', function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog){
         if(err){
             res.redirect('/blogs');
